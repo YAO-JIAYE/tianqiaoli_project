2d7a507048b033f8ed04fec247174cb02026a083
YAO <yaojiaye2022@gmail.com>
2025-07-17 18:01:37 +0900

    再config.ts中添加base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/tianqiaoli_project/',
   title: "グループ５８のサイト",
   description: "A VitePress Site",
   themeConfig: {
